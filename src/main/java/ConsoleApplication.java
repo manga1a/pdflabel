8008147de28e5dae609eb04736f17ea5288fcaaf
Mangala Kodagoda <mail.mangala@gmail.com>
2022-09-25 20:19:48 +1000

    Create multiple pages.

--- src/main/java/ConsoleApplication.java
+++ src/main/java/ConsoleApplication.java
@@ -1,4 +1,9 @@
+import org.apache.pdfbox.multipdf.LayerUtility;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.graphics.form.PDFormXObject;
 import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
 import org.apache.pdfbox.pdmodel.interactive.form.PDField;
 
@@ -9,18 +14,39 @@ public class ConsoleApplication {
 
     public static void main(String[] args) {
        try(InputStream is = ConsoleApplication.class.getClassLoader().getResourceAsStream("SsccLabel.pdf")) {
-           PDDocument document = PDDocument.load(is);
-           PDAcroForm form = document.getDocumentCatalog().getAcroForm();
-           PDField title = form.getField("title");
-           title.setValue("Hello World");
+           try(PDDocument source = PDDocument.load(is)) {
+               try(PDDocument target = new PDDocument()) {
+                   LayerUtility layerUtility = new LayerUtility(target);
+                   PDFormXObject formX = layerUtility.importPageAsForm(source, 0);
+                   for(int i=0; i < 3; ++i) {
+                       createPage(formX, target);
+                   }
 
-           PDField sscc = form.getField("sscc");
-           sscc.setValue("1234567890");
-           form.flatten();
-           document.save("./target/sscc-label-test.pdf");
-           document.close();
+                   target.save("./target/sscc-label-test.pdf");
+               }
+           }
+//           PDDocument document = PDDocument.load(is);
+//           PDAcroForm form = document.getDocumentCatalog().getAcroForm();
+//           PDField title = form.getField("title");
+//           title.setValue("Hello World");
+//
+//           PDField sscc = form.getField("sscc");
+//           sscc.setValue("1234567890");
+//           form.flatten();
+//           document.save("./target/sscc-label-test.pdf");
+//           document.close();
+
        } catch (IOException e) {
            e.printStackTrace();
        }
+    }
+
+    private static void createPage(PDFormXObject formX, PDDocument doc) throws IOException {
+        PDPage page = new PDPage(PDRectangle.A6);
+        doc.addPage(page);
+
+        try(PDPageContentStream contents = new PDPageContentStream(doc, page)) {
+            contents.drawForm(formX);
+        }
     }
 }
