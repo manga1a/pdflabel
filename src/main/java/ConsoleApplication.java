e7559a96718a756d2372094e60dbffe58e2a55e1
Mangala Kodagoda <mail.mangala@gmail.com>
2022-09-28 21:45:07 +1000

    Set working directory of FlyingSaucer work while running from IDE.

--- src/main/java/ConsoleApplication.java
+++ src/main/java/ConsoleApplication.java
@@ -1,26 +1,58 @@
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
-import org.apache.pdfbox.pdmodel.interactive.form.PDField;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.w3c.tidy.Tidy;
+import org.xhtmlrenderer.pdf.ITextRenderer;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.nio.file.FileSystems;
 
 public class ConsoleApplication {
+    private static final String UTF_8 = "UTF-8";
 
-    public static void main(String[] args) {
-       try(InputStream is = ConsoleApplication.class.getClassLoader().getResourceAsStream("SsccLabel.pdf")) {
-           PDDocument document = PDDocument.load(is);
-           PDAcroForm form = document.getDocumentCatalog().getAcroForm();
-           PDField title = form.getField("title");
-           title.setValue("Hello World");
+    public static void main(String[] args) throws UnsupportedEncodingException, MalformedURLException {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding(UTF_8);
 
-           PDField sscc = form.getField("sscc");
-           sscc.setValue("1234567890");
-           form.flatten();
-           document.save("./target/sscc-label-test.pdf");
-           document.close();
-       } catch (IOException e) {
-           e.printStackTrace();
-       }
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+
+        Context context = new Context();
+        context.setVariable("name", "Mangala");
+
+        String html = templateEngine.process("template", context);
+        String xhtml = toXhtml(html);
+
+        String baseUrl = FileSystems
+                .getDefault()
+                .getPath("src", "main", "resources")
+                .toUri()
+                .toURL()
+                .toString();
+
+        try(OutputStream outputStream = new FileOutputStream("./target/sscc-label-test.pdf")) {
+            ITextRenderer renderer = new ITextRenderer();
+            renderer.setDocumentFromString(xhtml, baseUrl);
+            renderer.layout();
+            renderer.createPDF(outputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String toXhtml(String html) throws UnsupportedEncodingException {
+        Tidy tidy = new Tidy();
+        tidy.setInputEncoding(UTF_8);
+        tidy.setOutputEncoding(UTF_8);
+        tidy.setXHTML(true);
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(html.getBytes(UTF_8));
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        tidy.parseDOM(inputStream, outputStream);
+        return outputStream.toString(UTF_8);
     }
 }
