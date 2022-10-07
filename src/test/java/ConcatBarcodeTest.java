ede8e4b8a953f4092dafcc327b2cc91747640b63
Mangala Kodagoda <mail.mangala@gmail.com>
2022-10-07 21:22:36 +1100

    Add unit tests for concat barcode.

--- src/test/java/ConcatBarcodeTest.java
+++ src/test/java/ConcatBarcodeTest.java
@@ -8,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ConcatBarcodeTest {
     private static DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
     @Test
     public void testBestBeforeWithBatch() throws ParseException {
         PalletData palletData = new PalletData();
@@ -18,5 +19,39 @@ public class ConcatBarcodeTest {
 
         String actual = ConsoleApplication.getConcatBarcode(palletData);
         assertEquals("02093123450000053720 1505120110246813", actual);
+    }
+
+    @Test
+    public void testBestBeforeWithoutBatch() throws ParseException {
+        PalletData palletData = new PalletData();
+        palletData.setGtin("09312345000005");
+        palletData.setCount("20");
+        palletData.setBestBeforeDate(dateFormat.parse("2022-10-01"));
+
+        String actual = ConsoleApplication.getConcatBarcode(palletData);
+        assertEquals("02093123450000053720 15221001", actual);
+    }
+
+    @Test
+    public void testUseByWithBatch() throws ParseException {
+        PalletData palletData = new PalletData();
+        palletData.setGtin("09312345000005");
+        palletData.setCount("999");
+        palletData.setUseByDate(dateFormat.parse("2020-01-30"));
+        palletData.setBatchNumber("111111");
+
+        String actual = ConsoleApplication.getConcatBarcode(palletData);
+        assertEquals("020931234500000537999 1720013010111111", actual);
+    }
+
+    @Test
+    public void testUseByWithoutBatch() throws ParseException {
+        PalletData palletData = new PalletData();
+        palletData.setGtin("09312345000005");
+        palletData.setCount("999");
+        palletData.setUseByDate(dateFormat.parse("2020-01-30"));
+
+        String actual = ConsoleApplication.getConcatBarcode(palletData);
+        assertEquals("020931234500000537999 17200130", actual);
     }
 }
